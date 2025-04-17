f32ce8b157a2df52bb3c952f912336c1f5a248cf
himansig7 <himansig7@gmail.com>
2025-04-17 00:17:28 -0700

    setting up sql

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -31,9 +31,27 @@ export async function fetchDriveItems(type = "all") {
     return { success: false, data: [] };
   }
 }
-
-
 //TODO add fetchDriveSearch(query) and fetchDriveRead(fileId)
+
+///////////
+// SQL Tools
+///////////
+
+export async function fetchSQLRows() {
+  try {
+    const response = await axios.post("http://localhost:5100/tool/sql_query", {
+      query: "SELECT * FROM users",
+      params: [],
+    });
+
+    return { success: true, data: response.data.rows };
+  } catch (error) {
+    console.error("Error fetching SQL rows:", error);
+    return { success: false, data: [] };
+  }
+}
+
+
 
 
 
